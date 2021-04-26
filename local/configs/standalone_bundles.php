5e738c2f32cada459843511748d52392522b2e08
Fedor <funtik21975@gmail.com>
2021-04-26 10:00:07 +0300

    ApiExceptionBundle вынесены в отдельный пакет.

--- local/configs/standalone_bundles.php
+++ local/configs/standalone_bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Local\Bundles\CustomArgumentResolverBundle\CustomArgumentResolverBundle::class => ['all' => true],
-    Local\Bundles\ApiExceptionBundle\M6WebApiExceptionBundle::class => ['all' => true],
     // Local\Bundles\ApiDtoConvertorBundle\ApiDtoConvertorBundle::class => ['all' => true],
     Local\Bundles\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
     Local\Bundles\SymfonyMiddlewareBundle\MiddlewareBundle::class => ['all' => true],

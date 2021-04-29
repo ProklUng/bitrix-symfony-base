dd688cff5e157d7b3797283b65d35c28dae398fb
Fedor <funtik21975@gmail.com>
2021-04-29 16:38:29 +0300

    Чистка.

--- local/configs/standalone_bundles.php
+++ local/configs/standalone_bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Local\Bundles\CustomArgumentResolverBundle\CustomArgumentResolverBundle::class => ['all' => true],
-    // Local\Bundles\ApiDtoConvertorBundle\ApiDtoConvertorBundle::class => ['all' => true],
     Prokl\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
     Local\Bundles\SymfonyMiddlewareBundle\MiddlewareBundle::class => ['all' => true],
     Local\Bundles\TaskSchedulerBundle\RewieerTaskSchedulerBundle::class => ['all' => true],

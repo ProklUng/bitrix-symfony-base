43c3efc57d2f55865005c3ff0a298a5cd3aa4622
ProklUng <funtik21975@gmail.com>
2021-05-08 09:11:08 +0300

    Разгрузка.

--- local/functions/helpers.php
+++ local/functions/helpers.php
@@ -1,21 +1,5 @@
 <?php
 
-use Local\Tests\FixtureGenerator\FixtureManager;
-
-if (!function_exists('fixture')) {
-    function fixture()
-    {
-        return containerLaravel()->make(FixtureManager::class);
-    }
-}
-
-if (!function_exists('faker')) {
-    function faker()
-    {
-        return FixtureManager::getFaker();
-    }
-}
-
 if (!function_exists('bxConfig')) {
     function bxConfig(string $key)
     {

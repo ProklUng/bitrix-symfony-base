f4de60a5f73010fc9c83c2b98fcc79a5cc24e869
ProklUng <funtik21975@gmail.com>
2021-05-31 19:57:00 +0300

    composer.json

--- local/php_interface/symfony_events.php
+++ local/php_interface/symfony_events.php
@@ -1,8 +1,12 @@
 <?php
 // Инициализация событий Symfony.
 
-use Local\Util\SymfonyEvents\Events;
+use Prokl\BitrixOrdinaryToolsBundle\Services\SymfonyEvents\CustomEvents;
 
-$symfonyEvents = new Events();
+if (class_exists(CustomEvents::class)) {
+    /** @var CustomEvents $symfonyEvents */
+    $symfonyEvents = container()->get('bitrix_ordinary_tools.custom_event_dispatcher');
 // Слушатели по умолчанию.
-$symfonyEvents->applyListeners();
+    $symfonyEvents->applyListeners();
+}
+

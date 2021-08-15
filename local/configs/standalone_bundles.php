71a1408eafae2351d81ddce9292d9e16dd9f9d52
ProklUng <funtik21975@gmail.com>
2021-08-15 15:07:26 +0300

    Базовый Твиг вынесен в отдельный бандл

--- local/configs/standalone_bundles.php
+++ local/configs/standalone_bundles.php
@@ -3,15 +3,15 @@
 return [
     Prokl\CustomFrameworkExtensionsBundle\CustomFrameworkExtensionsBundle::class => ['all' => true],
     Prokl\CustomArgumentResolverBundle\CustomArgumentResolverBundle::class => ['all' => true],
+    Prokl\BitrixTwigBundle\BitrixTwigBundle::class => ['all' => true],
+    Prokl\FrameworkExtensionBundle\FrameworkExtensionBundle::class => ['all' => true],
     Prokl\BitrixSymfonyRouterBundle\SymfonyRouterBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     Prokl\BitrixIblockElementValidatorBundle\BitrixIblockElementValidatorBundle::class => ['all' => true],
-    Prokl\FrameworkExtensionBundle\FrameworkExtensionBundle::class => ['all' => true],
     Prokl\StaticPageMakerBundle\StaticPageMakerBundle::class => ['all' => true],
     Prokl\BitrixWebformBundle\BitrixWebformBundle::class => ['all' => true],
     Prokl\FacadeBundle\FacadeBundle::class => ['all' => true],
     Prokl\TwigExtensionsPackBundle\TwigExtensionsPackBundle::class => ['all' => true],
-    Prokl\BitrixOrdinaryToolsBundle\BitrixOrdinaryToolsBundle::class => ['all' => true],
     Prokl\BundleMakerBundle\BundleMakerBundle::class => ['dev' => true],
 ];
